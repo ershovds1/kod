0af8e74b4095684a43118884275504595ac76d4f
Dmitry <ershovds1@gmail.com>
2022-10-04 01:30:25 +0300

    Большее меньшее число

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int a, b;
+Console.WriteLine("Введите два числа : ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("{0} наибольшее число", a);
+    Console.WriteLine("{0} наименьшее число", b);
+}
+else
+{
+    Console.WriteLine("{0} наибольшее число ", b);
+    Console.WriteLine("{0} наименьшее число ", a);
+}
