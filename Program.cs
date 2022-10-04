35ac76af39e96cb4de4209e8c1c6c37804a8c634
Dmitry <ershovds1@gmail.com>
2022-10-04 11:23:45 +0300

    Определение четности числа

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Программа, которая на вход принимает число и выдаёт, является ли число чётным
+int a;
+Console.WriteLine("Введите число");
+a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("Четное число");
+}
+else
+{
+    Console.WriteLine("Нечетное число");
+}
