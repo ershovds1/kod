9303b62f5aa5dda02d0106d9802a82f14204a0ef
Dmitry <ershovds1@gmail.com>
2022-10-05 18:18:26 +0300

    Третья цифра

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
-﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-int a, result;
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int a;
 
-Console.WriteLine("Введите трехзначное число");
+Console.WriteLine("Введите число");
 int.TryParse(Console.ReadLine()!, out a);
-a = ((a / 10) % 10);
-
-Console.WriteLine($"Вторая цифра {a}");
+int count = a.ToString().Length;
+if (count >= 3)
+{
+    Console.WriteLine($"Третья цифра {a % 10}");
+}
+else
+    Console.WriteLine("Третьей цифры нет");
