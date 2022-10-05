4f520321b5914f154ced43985f414752111d5390
Dmitry <ershovds1@gmail.com>
2022-10-05 18:34:06 +0300

    Выходной

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
-﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 int a;
 
-Console.WriteLine("Введите число");
+Console.WriteLine("Введите число от 1 до 7: ");
+//a = Convert.ToInt32(Console.ReadLine());
 int.TryParse(Console.ReadLine()!, out a);
-int count = a.ToString().Length;
-if (count >= 3)
+if (a == 6)
 {
-    Console.WriteLine($"Третья цифра {a % 10}");
+    Console.WriteLine("Выходной");
+}
+else if (a == 7)
+{
+    Console.WriteLine("Выходной");
 }
 else
-    Console.WriteLine("Третьей цифры нет");
+    Console.WriteLine("Рабочий день");
