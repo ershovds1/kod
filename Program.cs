803cd451fbda8a65dc428ee95bc1e15d36bf3e34
Дмитрий <114745238+ershovds1@users.noreply.github.com>
2022-10-07 12:08:27 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Напишите программу, которая принимает на вход пятизначное число и 
+//проверяет, является ли оно палиндромом;
+
+Console.WriteLine("Введите пятизначное число");
+
+string a = Console.ReadLine();
+int len = a.Length;
+
+if (len == 5)
+{
+    if (a[0] == a[4] && a[1] == a[3])
+    {
+        Console.WriteLine($"{a} - Число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"{a} - Число не является палиндромом");
+    }
+}
+else
+{
+    Console.WriteLine($"ОШИБКА: {a} - не является пятизначным");
+}
+
