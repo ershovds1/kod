ad1ba876e272e71020341f435bef09b4478fd6e1
Дмитрий <114745238+ershovds1@users.noreply.github.com>
2022-10-07 12:59:35 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int a = ReadInt("Введите число N: ");
+for (int i = 1; i <= a; i++)
+{
+    Console.Write($"{i * i * i} ");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
