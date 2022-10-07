20b7b2816e6a17351cdc3c92f7e176f7202e97fd
Дмитрий <114745238+ershovds1@users.noreply.github.com>
2022-10-07 12:47:18 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
 //Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
 
-int x1 = ReadInt("Введите координату X1: ");
-int y1 = ReadInt("Введите координату Y1: ");
-int x2 = ReadInt("Введите координату X2: ");
-int y2 = ReadInt("Введите координату Y2: ");
-int z1 = ReadInt("Введите координату Z1: ");
-int z2 = ReadInt("Введите координату Z2: ");
+Console.Writeln("Введите координату x1: ");
+int.TryParse(Console.ReadLine()!, out x1);
+
+Console.Writeln("Введите координату y1: ");
+int.TryParse(Console.ReadLine()!, out y1);
+
+Console.Writeln("Введите координату x2: ");
+int.TryParse(Console.ReadLine()!, out x2);
+
+Console.Writeln("Введите координату y2: ");
+int.TryParse(Console.ReadLine()!, out y2);
+
+Console.Writeln("Введите координату z1: ");
+int.TryParse(Console.ReadLine()!, out z1);
+
+Console.Writeln("Введите координату z2: ");
+int.TryParse(Console.ReadLine()!, out z2);
 
 int A = x2 - x1;
 int B = y2 - y1;
@@ -13,10 +24,3 @@ int C = z1 - z2;
 
 double length = Math.Sqrt(A * A + B * B + C * C);
 Console.WriteLine($"Длинна отрезка {length}");
-
-// Функция ввода сообщения
-int ReadInt(string message)
-{
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
-}
