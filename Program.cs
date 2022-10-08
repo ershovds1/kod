f2da37f1f425098491465f3e3f13d73e4ca57149
Дмитрий <114745238+ershovds1@users.noreply.github.com>
2022-10-08 08:42:09 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int number = vvod("Введите число: ");
+int len = Num(number);
+Sum(number, len);
+
+// Ввод
+int vvod(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Подсчета цифр в числе
+int Num(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
