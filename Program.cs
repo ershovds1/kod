a63271bba738756f3a4450804bc799914a2076ec
Дмитрий <114745238+ershovds1@users.noreply.github.com>
2022-10-11 15:00:36 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int size = ReadInt("Введите размерность массива: ");
+int[] numbers = new int[size];
+
+ArrayNumbers(numbers);
+PrintArray(numbers);
+int result = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] % 2 == 0)
+    {
+        result++;
+    }
+}
+if (result % 10 == 1)
+{
+    Console.WriteLine($"В массиве {result} четное число");
+}
+if (result % 10 == 2 || result % 10 == 3 || result % 10 == 4)
+{
+    Console.WriteLine($"В массиве {result} четных числа");
+}
+else
+
+    Console.WriteLine($"В массиве {result} четных чисел");
+
+// Методы
+//Заполнение массива
+void ArrayNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(99, 999);
+    }
+}
+//Вывод массива
+void PrintArray(int[] array) 
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+//Функция ввода
+int ReadInt(string message) 
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
