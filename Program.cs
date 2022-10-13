56ab75c5e286e610fc93f831a1c474959832d4e6
Дмитрий <114745238+ershovds1@users.noreply.github.com>
2022-10-13 09:14:38 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+double b1 = ReadInt("Введите b1: ");
+double b2 = ReadInt("Введите b2: ");
+double k1 = ReadInt("Введите k1: ");
+double k2 = ReadInt("Введите k2: ");
+
+double first = (b1 - b2) / (k2 - k1);
+double second = (k2 * first) + b2;
+
+Console.WriteLine($"({first}:{second})");
+
+// Метод
+double ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
