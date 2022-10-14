e3faa2d153e33a2207769c4d2caa2a3a85ddfdeb
Дмитрий <114745238+ershovds1@users.noreply.github.com>
2022-10-14 14:46:57 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+int rows = ReadInt("Введите количество строк: ");
+int colums = ReadInt("Введите количество столбцов: ");
+double[,] numbers = new double[rows, colums];
+
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+void FillArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)//GetLength(0) - строки
+    {
+        for (int j = 0; j < array.GetLength(1); j++)//GetLength(1) - столбцы
+        {
+            array[i, j] = new Random().NextDouble();
+        }
+    }
+}
+
+void PrintArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)//GetLength(0) - строки
+    {
+        for (int j = 0; j < array.GetLength(1); j++)//GetLength(1) - столбцы
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
