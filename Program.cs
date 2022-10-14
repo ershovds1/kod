b5ebe94cd8cc48ecb178e4a5a38cb2e5a773a070
Дмитрий <114745238+ershovds1@users.noreply.github.com>
2022-10-14 14:50:26 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+
+int rows = ReadInt("Введите количество строк: ");
+int colums = ReadInt("Введите количество столбцов: ");
+int[,] numbers = new int[rows, colums];
+int idRows = ReadInt("Введите строку: ");
+int idColums = ReadInt("Введите столбец: ");
+
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+if ((idRows < rows)&(idColums < colums))
+{
+    Console.WriteLine(numbers[idRows, idColums]);
+}
+else
+{
+    Console.WriteLine("Такого числа в массиве нет");
+}
+
+void FillArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)//GetLength(0) - строки
+    {
+        for (int j = 0; j < array.GetLength(1); j++)//GetLength(1) - столбцы
+        {
+            array[i, j] = new Random().Next(-9, 9);
+        }
+    }
+}
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)//GetLength(0) - строки
+    {
+        for (int j = 0; j < array.GetLength(1); j++)//GetLength(1) - столбцы
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
